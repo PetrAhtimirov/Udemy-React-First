19c823bfd835c9095f5ccd9872c3869ed0c32df3
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-08-13 19:03:25 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import './App.css';
 
 const Header = () => {
@@ -41,7 +41,9 @@ function Btn() {
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <StrictMode>
+        <Header/>
+      </StrictMode>
       <Field/>
       <Btn/>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Header} from './App';
 
 const elem = (
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
