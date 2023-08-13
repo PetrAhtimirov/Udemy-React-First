b2c524e5f310c57d5f65db87c16a703c3025fb78
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2023-08-13 20:49:29 +0300

    add new base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import {Header} from './App';
-
-const elem = (
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
